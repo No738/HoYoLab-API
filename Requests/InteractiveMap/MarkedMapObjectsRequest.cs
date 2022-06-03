3c6651dfde6c70d65f50abcee7463378673af0e6
No. 738 <no738.000@gmail.com>
2022-06-03 22:19:53 +1000

    fixed marked map objects cache

--- Requests/InteractiveMap/MarkedMapObjectsRequest.cs
+++ Requests/InteractiveMap/MarkedMapObjectsRequest.cs
@@ -2,7 +2,7 @@
 {
     internal sealed class MarkedMapObjectsRequest : DefaultRequest
     {
-        private static readonly string MarkedMapObjectsUrl = "https://sg-public-api-static.hoyolab.com/common/map_user/ys_obc/v1/map/point/mark_map_point_list";
+        private static readonly string MarkedMapObjectsUrl = "https://sg-public-api.hoyolab.com/common/map_user/ys_obc/v1/map/point/mark_map_point_list";
 
         public MarkedMapObjectsRequest(HoyolabAccount account, InteractiveMap.Location mapLocation) : base(account)
         {
